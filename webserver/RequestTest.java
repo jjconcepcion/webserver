24520ddef82566be5c3518a6de65042c3929eb6c
Jan Joseph Concepcion <jconcep2@mail.sfsu.edu>
2017-02-10 19:21:47 -0800

    Create test for Request class

---
+++ webserver/RequestTest.java
@@ -0,0 +1,33 @@
+import java.net.*;
+import java.io.*;
+
+public class RequestTest {
+  public static final int DEFAULT_PORT = 3100;
+
+  public static void main( String[] args ) throws IOException {
+
+    ServerSocket socket = new ServerSocket( DEFAULT_PORT );
+    Socket client = null;
+    Request request = null;
+
+    client = socket.accept();
+    request = new Request( client.getInputStream() );
+    request.parse();
+    
+    printStartLine( request );
+    
+    int contentLength = Integer.parseInt( 
+      request.lookupHeader( "ContentLength" )
+    );
+    
+    System.out.println( "Request has body = " + ( contentLength > 0 ));
+    
+    client.close();
+  }
+  
+  public static void printStartLine(Request request) {
+    System.out.println( ">>Displaying request start line:" );
+    System.out.print( request.getVerb() + " " + request.getUri() + " " );
+    System.out.println( request.getHttpVersion() );
+  }
+}
