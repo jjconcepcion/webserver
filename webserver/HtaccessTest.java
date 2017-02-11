78d456a2cdea5e82301698e376cbaf01f4ce9bac
Daniel Tri Nguyen <dnguye26@mail.sfsu.edu>
2017-02-11 06:17:31 -0800

    Implemented test class for Htaccess

---
+++ webserver/HtaccessTest.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class HtaccessTest {
+  public static void main( String[] args) throws FileNotFoundException {
+    Htaccess htaccess = null;
+
+    try {
+      htaccess = new Htaccess( args[0] );
+    } catch ( ArrayIndexOutOfBoundsException | FileNotFoundException e) {
+      System.out.println( e + "\nPass filename of htaccess file as argument");
+      System.exit(0);
+    }
+    
+    try {
+      htaccess.load();
+      System.out.println( "\n Loaded htaccess file successfully ");
+    } catch ( IOException e ) {
+      System.out.println( "Failed to load htaccess file: " + e );
+      System.exit(0);
+    }
+
+    System.out.println( ">>> Printing out AuthUserFile Path <<<" );
+    System.out.println( htaccess.getAuthUserFilePath() );
+    System.out.println( ">>> Printing out AuthType <<<" );
+    System.out.println( htaccess.getAuthType() );
+    System.out.println( ">>> Printing out AuthName <<<" );
+    System.out.println( htaccess.getAuthName() );
+    System.out.println( ">>> Printing out Require <<<" );
+    System.out.println( htaccess.getRequire() );
+
+    // System.out.println( ">>> Testing isAuthorized <<<" );
+    // System.out.println( htaccess.isAuthorized( "jrob" , 
+    //   "{SHA}cRDtpNCeBiql5KOQsKVyrA0sAiA=" ) );
+  }
+}
