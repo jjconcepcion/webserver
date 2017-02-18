56e402d61c483939da7d297a974ae270232cdb85
Daniel Tri Nguyen <dnguye26@mail.sfsu.edu>
2017-02-18 07:53:49 -0800

    Implemented Resource class, still need to implement isProtected and test all cases for absolutePath

--- webserver/Resource.java
+++ webserver/Resource.java
@@ -1,26 +1,109 @@
 import java.io.*;
+import java.util.StringTokenizer;
+import java.util.HashMap;
+import java.net.*;
+import java.util.ListIterator;
 
 public class Resource {
+  private String absolutePath;
+  private HttpdConf httpdConf;
+  private String requestUri;
+  private String path;
+  private String lastSegment;
+  private String firstSegment;
+  private File file;
 
-  private String uri;
-  private HttpdConf config;
+  public Resource( String uri, HttpdConf conf ) {
+    requestUri = uri;
+    httpdConf = conf;
+    file = new File(requestUri);
 
-  public Resource( String uri, HttpdConf config ) {
-
-    uri = uri;
-    config = config;
-    
   }
 
   public String absolutePath() {
-    return path;
+    Resource resource = new Resource( requestUri, httpdConf);
+    resource.parseUri();
+    
+    if ( resource.isAlias() ) {
+      absolutePath = httpdConf.lookupAlias( firstSegment);
+      if ( isDirectory() ) {
+        if ( httpdConf.getDirectoryIndexes().equals(null) ) {
+          return absolutePath;
+        } else {
+          absolutePath += httpdConf.getDirectoryIndexes().next();
+        }
+      }
+    }
+    
+    if ( resource.isScriptAlias() ) {
+      absolutePath = httpdConf.lookupScriptAlias( firstSegment );
+      if ( isDirectory() ) {
+        if ( httpdConf.getDirectoryIndexes().equals(null) ) {
+          return absolutePath;
+        } else {
+          absolutePath += httpdConf.getDirectoryIndexes().next();
+        }
+      }
+    }
+
+    if ( isDirectory() ) {
+      if ( httpdConf.getDirectoryIndexes().equals(null) ) {
+        return absolutePath;
+      } else {
+        absolutePath += httpdConf.getDirectoryIndexes().next();
+      }
+    } else {
+      absolutePath = httpdConf.getDocumentRoot() + uri;
+    }
+    return absolutePath;
   }
 
-  public boolean isScript() {
-    return false;
+  public boolean isAlias() {
+    return httpdConf.aliasesContainsKey( firstSegment );
+  }
+
+  public boolean isScriptAlias() {
+    return httpdConf.scriptedAliasesContainsKey( firstSegment );
+
   }
 
   public boolean isProtected() {
     return false;
+  }
+
+  public void parseUri() {
+    // try {
+      // URI uri = new URI(requestUri); 
+      // path = uri.getPath();
+      // lastSegment = path.substring( path.lastIndexOf( '/' ) + 1 );
+      
+      // File file = new File(requestUri);
+      path = file.getPath();
+      firstSegment = path.replaceAll(file.getName(),"");
+      lastSegment = path.substring( path.lastIndexOf( '/' ) + 1 );
+
+    // } catch (URISyntaxException e) {} 
+
+  }
+  public boolean isFile() {
+    File file = new File(absolutePath);
+    return file.isFile();
+  }
+
+  public boolean isDirectory() {
+    File file = new File(absolutePath);
+    return file.isDirectory();
+  }
+
+  public String getPath() {
+    return path;
+  }
+
+  public String getFirstSegment() {
+    return firstSegment;
+  }
+
+  public String getLastSegment() {
+    return lastSegment;
   }
 }
