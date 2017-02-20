004d817caa0aca58779d242d8fe03c0fa90b1e48
Jan Joseph Concepcion <janjoseph.concepcion@gmail.com>
2017-02-19 22:27:39 -0800

    Remove main() and move server startup

--- webserver/Server.java
+++ webserver/Server.java
@@ -3,25 +3,22 @@ import java.io.*;
 import java.lang.Thread;
 
 public class Server {
-  private static final String HTTPD_CONF_PATH = "conf/httpd.conf";
-  private static final String MIME_TYPES_PATH = "conf/mime.types";
+  private static final String HTTPD_CONF_PATH = "./conf/httpd.conf";
+  private static final String MIME_TYPES_PATH = "./conf/mime.types";
   private static int listenPort;
   private static HttpdConf configuration;
   private static MimeTypes mimeTypes;
   
-  public static void main( String[] args ) throws IOException {
-    start();
+  public Server() throws IOException {
+    configuration = new HttpdConf( HTTPD_CONF_PATH );
+    mimeTypes = new MimeTypes( MIME_TYPES_PATH );
+  
+    configuration.load();
+    mimeTypes.load();
   }
   
   public static void start() throws IOException {
-    
-    try {
-      loadServerConfiguration();
-    } catch( IOException exception ) {
-      System.out.println( exception.getMessage() );
-      System.exit(1);
-    }
-    
+    listenPort = configuration.getListenPort();
     ServerSocket socket = new ServerSocket( listenPort );
     Socket client = null;
     Thread worker = null;
@@ -33,14 +30,5 @@ public class Server {
       worker.start();
       //client.close();
     }
-  }
-  
-  public static void loadServerConfiguration() throws IOException {
-    configuration = new HttpdConf( HTTPD_CONF_PATH );
-    mimeTypes = new MimeTypes( MIME_TYPES_PATH );
-  
-    configuration.load();
-    mimeTypes.load();
-    listenPort = configuration.getListenPort();
   }
 }
