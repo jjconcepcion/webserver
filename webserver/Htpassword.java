40c39f2fcfd7b08a041b7c8a076c69f16dfb220c
Jan Joseph Concepcion <janjoseph.concepcion@gmail.com>
2017-02-21 02:38:07 -0800

    Add provided Htpassword class

--- webserver/Htpassword.java
+++ webserver/Htpassword.java
@@ -1,62 +1,61 @@
-import java.io.IOException;
-import java.io.FileNotFoundException;
+package server.configuration;
+
 import java.util.HashMap;
-import java.util.StringTokenizer;
+import java.util.Base64;
+import java.nio.charset.Charset;
+import java.security.MessageDigest;
+
+import java.io.IOException;
 
 public class Htpassword extends ConfigurationReader {
-  private HashMap<String, String> users;
-  private String delimiter = ":{SHA}";
+  private HashMap<String, String> passwords;
 
+  public Htpassword( String filename ) throws IOException {
+    super( filename );
+    System.out.println( "Password file: " + filename );
 
-  public Htpassword (String fileName ) throws FileNotFoundException {
-    super( fileName );
-    users = new HashMap<String,String>();
-
+    this.passwords = new HashMap<String, String>();
+    this.load();
   }
 
-  public void load() throws IOException {
-    String line = null;
+  protected void parseLine( String line ) {
+    String[] tokens = line.split( ":" );
 
-    while( hasMoreLines() ) {
-      line = nextLine();
-      if ( line.startsWith( "#" ) || line.isEmpty() ) {
-        continue;
-      }
-      parse(line);
+    if( tokens.length == 2 ) {
+      passwords.put( tokens[ 0 ], tokens[ 1 ].replace( "{SHA}", "" ).trim() );
     }
   }
 
-  public void parse( String line ) {
-    String [] lineParts = line.split( ":" );
-    StringTokenizer tokens = new StringTokenizer ( line, ":" );
-    String username, password;
+  public boolean isAuthorized( String authInfo ) {
+    // authInfo is provided in the header received from the client
+    // as a Base64 encoded string.
+    String credentials = new String(
+      Base64.getDecoder().decode( authInfo ),
+      Charset.forName( "UTF-8" )
+    );
 
-    username = lineParts[0];
-    password = lineParts[1];
+    // The string is the key:value pair username:password
+    String[] tokens = credentials.split( ":" );
 
-    users.put( username, password );
-
+    // TODO: implement this
   }
 
-  public boolean isAuthorized( String username, String password ) {
-    return ( users.get( username ).equals( password ) );
+  private boolean verifyPassword( String username, String password ) {
+    // encrypt the password, and compare it to the password stored
+    // in the password file (keyed by username)
+    // TODO: implement this
   }
 
-  public void getUserNames() {
-    System.out.println( users.keySet() );
-  }
+  private String encryptClearPassword( String password ) {
+    // Encrypt the cleartext password (that was decoded from the Base64 String
+    // provided by the client) using the SHA-1 encryption algorithm
+    try {
+      MessageDigest mDigest = MessageDigest.getInstance( "SHA-1" );
+      byte[] result = mDigest.digest( password.getBytes() );
 
-  public void getPasswords() {
-    System.out.println( users.values()) ;
-  }
-
-
-  public void printHashMap() {
-    for ( String key : users.keySet() ) {
-      String value = users.get(key).toString();
-      System.out.println( "username: " + key + " " + "password: " + value);
+     return Base64.getEncoder().encodeToString( result );
+    } catch( Exception e ) {
+      return "";
     }
   }
-
-
 }
