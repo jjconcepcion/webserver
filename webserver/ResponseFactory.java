7b944abe12f8c5ca46a4036456987518d89e61b7
Jan Joseph Concepcion <janjoseph.concepcion@gmail.com>
2017-02-20 22:59:02 -0800

    WIP: ResponseFactory
    Use overloaded getResponse to handle certain exceptions

--- webserver/ResponseFactory.java
+++ webserver/ResponseFactory.java
@@ -9,23 +9,10 @@ public class ResponseFactory {
     Response response = null;
     
     if( exception instanceof BadRequestException ) {
-      //response = new BadRequestException( resource );
-    } else if( exception instanceof CreatedException ) {
-      //response = new CreatedResponse( resource );
-    } else if( exception instanceof ForbiddenException ) {
-      //response = new ForbiddenResponse( resource );
-    } else if( exception instanceof InternalServerErrorException ) {
-      //response = new InternalServerErrorResponse( resource );
-    } else if( exception instanceof NoContentException ) {
-      //response = new NoContentResponse( resource );
-    } else if( exception instanceof NotFoundException ) {
-      //response = new NotFoundResponse( resource );
-    } else if( exception instanceof NotModifiedException ) {
-      //response = new NotModifiedResponse( resource );
-    } else if( exception instanceof OKException ) {
-      response = new OKResponse( resource );
     } else if( exception instanceof UnauthorizedException) {
-      //response = new UnauthorizedResponse( resource );
+    } else if( exception instanceof ForbiddenException ) {
+    } else if( exception instanceof NotFoundException ) {
+    } else if( exception instanceof InternalServerErrorException ) {
     } 
     
     return response;
@@ -34,20 +21,13 @@ public class ResponseFactory {
   public static Response getResponse( Request request, Resource resource ) 
       throws IOException {
     Response response = null;
-    String requestMethod = request.getVerb();
-    File file;
+    String requestMethod;
     
     requestMethod = request.getVerb();
-    file = resource.getFile();
-    //file = new File( resource.getAbsolutePath() );
-    
-    System.out.println("FACTORY");
-    System.out.println(file);
     
     if(requestMethod.equals("GET")) {
       response = new OKResponse( resource );
       response.setRequestMethod(requestMethod );
-      //response.setBodyDataFrom( file );
     }
     
     return response;
