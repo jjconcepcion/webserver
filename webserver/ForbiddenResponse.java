e0f3beabc3a36ca7b76ef3acb340edd1e1c343f0
Jan Joseph Concepcion <janjoseph.concepcion@gmail.com>
2017-02-23 06:12:08 -0800

    Create ForbiddenResponse

---
+++ webserver/ForbiddenResponse.java
@@ -0,0 +1,35 @@
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class ForbiddenResponse extends Response {
+  public ForbiddenResponse( Resource resource ) {
+    super( resource );
+    this.code = 403;
+    this.reasonPhrase = "ForbiddenResponse";
+  }
+  
+  public void send( OutputStream outputStream ) throws IOException {
+    BufferedWriter out = new BufferedWriter( 
+      new OutputStreamWriter( outputStream )
+    );
+    
+    String body = "<html><head><title>403 Forbidden</title></head><body>" 
+      + "<h1>401 - Forbidden</h1></body></html>";
+    
+    this.sendCommonPreamble( out );
+    this.sendSetHeaders( out );
+    this.sendHeaderLine( out, "Content-Type", "text/html" );
+    this.sendHeaderLine( out, "Content-Length", 
+      String.valueOf( body.length() ) 
+    );
+   
+    out.write(this.CRLF);
+    out.flush();
+    
+    out.write( body );
+    out.flush();
+  }
+  
+}
