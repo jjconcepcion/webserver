cff8171d773c6aa970bdc02eaaccf087b1898a1b
Jan Joseph Concepcion <janjoseph.concepcion@gmail.com>
2017-02-24 01:02:21 -0800

    Created InternalServerErrorResponse

---
+++ webserver/InternalServerErrorResponse.java
@@ -0,0 +1,35 @@
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.BufferedWriter;
+import java.io.IOException;
+
+public class InternalServerErrorResponse extends Response {
+  public InternalServerErrorResponse( Resource resource ) {
+    super( resource );
+    this.code = 500;
+    this.reasonPhrase = "Internal Server Error";
+  }
+  
+  public void send( OutputStream outputStream ) throws IOException {
+    BufferedWriter out = new BufferedWriter( 
+      new OutputStreamWriter( outputStream )
+    );
+    
+    String body = "<html><head><title>500 Internal Server Error</title></head><body>" 
+      + "<h1>500 Internal Server Error</></body></html>";
+    
+    this.sendCommonPreamble( out );
+    this.sendHeaderLine( out, "Content-Type", "text/html" );
+    this.sendHeaderLine( out, "Content-Length", 
+      String.valueOf( body.length() ) 
+    );
+    this.sendHeaderLine( out, "Connection", "closed");
+   
+    out.write(this.CRLF);
+    out.flush();
+    
+    out.write( body );
+    out.flush();
+  }
+  
+}
